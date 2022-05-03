7fdd2768a5d5bb01cec24c2d25f16ae05b5edd79
Carlos Alberto López Mata <mata66666666666@gmail.com>
2022-05-03 16:37:16 -0500

    update

--- main.js
+++ main.js
@@ -9,8 +9,11 @@ function generatePDF(){
 function mostrarCertificados() {
     document.getElementById('certificados').style.display = 'block';
     document.getElementById('button-certification-on').style.display = 'none';
+    document.getElementById('button-certification-off').style.display = 'block'; 
 }
 function ocultarCertificados() {
     document.getElementById('certificados').style.display = 'none';
-    document.getElementById('button-certification-off').style.display = 'block';
-}
+    document.getElementById('button-certification-off').style.display = 'none';
+    document.getElementById('button-certification-on').style.display = 'block';
+} 
+/*YA SE ACTUALIZO?*/
